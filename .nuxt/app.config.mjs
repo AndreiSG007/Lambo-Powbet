891d9d920fd1e1bfb4ef69dfe2a143d77b26308d
AndreiSGOO7 <andrei@softgenius.com>
2023-03-02 15:44:16 +0200

    main push

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/andreisoftgenius/Documents/LAMBO/lambo-promo-PowBet/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
